a027320b82939094ea89b659e99ffcc854254dcd
tariqmanon <tmanon007@gmail.com>
2020-09-26 05:26:56 +0600

    latest update

---
+++ js/main.js
@@ -0,0 +1,46 @@
+$('.owl-carousel').owlCarousel({
+    margin: 10,
+    nav: true,
+    loop:true,
+    dots:false,
+    // navText:["<div class='nav-btn prev-slide'></div>","<div class='nav-btn next-slide'></div>"],
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
+});
+
+
+/// some script
+$(function () {
+      'use strict'
+
+    $("[data-trigger]").on("click", function(){
+        var trigger_id =  $(this).attr('data-trigger');
+        $(trigger_id).toggleClass("show");
+        $('body').toggleClass("offcanvas-active");
+    });
+
+    // close if press ESC button 
+    $(document).on('keydown', function(event) {
+        if(event.keyCode === 27) {
+           $(".navbar-collapse").removeClass("show");
+           $("body").removeClass("overlay-active");
+        }
+    });
+
+    // close button 
+    $(".btn-close").click(function(e){
+        $(".navbar-collapse").removeClass("show");
+        $("body").removeClass("offcanvas-active");
+    }); 
+
+
+})
